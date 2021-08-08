4070fe7cfec896e74c4b1dd84e54f84468926e1a
tongweiyang <tongweiyang01@163.com>
2021-08-08 14:54:58 +0800

    init

--- spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -76,19 +76,19 @@ public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements
 
 	/** Cache of singleton objects: bean name to bean instance. */
 	/**
-	 * 一级缓存
+	 * 一级缓存，里面存放的是半成品对象
 	 */
 	private final Map<String, Object> singletonObjects = new ConcurrentHashMap<>(256);
 
 	/** Cache of singleton factories: bean name to ObjectFactory. */
 	/**
-	 * 二级缓存
+	 * 三级缓存，使用三级缓存是为了解决aop的代理问题
 	 */
 	private final Map<String, ObjectFactory<?>> singletonFactories = new HashMap<>(16);
 
 	/** Cache of early singleton objects: bean name to bean instance. */
 	/**
-	 * 三级缓存
+	 * 二级缓存
 	 */
 	private final Map<String, Object> earlySingletonObjects = new ConcurrentHashMap<>(16);
 
